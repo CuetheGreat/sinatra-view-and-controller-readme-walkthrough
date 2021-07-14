ddd2c8ad3b7202ee7f6cbf7fa42de0328938d075
Clay Stewart <c.stewart89@gmail.com>
2021-07-14 12:27:57 -0400

    commit tree 1e3df68613d612c7b1d6922540df720ddccec277

--- app.rb
+++ app.rb
@@ -11,6 +11,14 @@ class App < Sinatra::Base
 
   get '/friends' do
     # Write your code here!
-
+    puts params
+    @friends = [
+      'Emily Wilding Davison',
+      'Harriet Tubman',
+      'Joan of Arc',
+      'Malala Yousafzai',
+      'Sojourner Truth',
+    ]
+    erb :friends
   end
 end
